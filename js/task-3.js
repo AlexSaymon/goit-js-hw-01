31e1fe72f0a283d9bd8c151be9fd16b8f62ef5c4
Oleksandr Kurchenko <151569683+AlexSaymon@users.noreply.github.com>
2024-02-23 12:33:25 -0800

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,6 @@
 function getElementWidth(content, padding, border) {
-  return Number.parseFloat(content) + (Number.parseFloat(padding) + Number.parseFloat(border)) * 2 + `px`;
+  return Number.parseFloat(content) + (Number.parseFloat(padding) + Number.parseFloat(border)) * 2;
 }
-
-getElementWidth();
 
 console.log(getElementWidth('50px', '8px', '4px')); // 74
 console.log(getElementWidth('60px', '12px', '8.5px')); // 101
